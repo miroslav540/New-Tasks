4e787aa361587c057e2ad4cc2f724b8f13d479fc
Miroslav <mir19972905@mai.ru>
2022-06-28 11:03:50 +0300

    10 done

---
+++ task10/Program.cs
@@ -0,0 +1,9 @@
+﻿void findSecondNumber(int start, int finish)
+{
+    int num = new Random().Next(start, finish);
+    Console.WriteLine(num);
+    int secondnum = num % 100 /10;
+    Console.WriteLine(secondnum);
+}
+
+findSecondNumber(100, 999);
