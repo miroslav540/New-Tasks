0775110a9c852e291e10b3bb3ad6c88d2e5a23e4
Miroslav <mir19972905@mai.ru>
2022-06-28 10:55:28 +0300

    do 15 task

---
+++ task15/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите день недели, чтобы узнать, выходной ли это.");
+int num = Convert.ToInt32(Console.ReadLine());
+string[] weekDays = new string[] { "Понедельник, нет", "Вторник, нет", "Среда, нет", "Четверг, нет", "Пятница, нет", "Суббота, да", "Воскресенье, да" };
+
+Console.WriteLine(weekDays[num-1]);
